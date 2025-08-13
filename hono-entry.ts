06ec1a0ab93d5d59038d20b59a1975f9635801a9
Bati <no-reply@batijs.dev>
2025-08-12 20:15:53 -0400

    scaffold Vike app with Bati

---
+++ hono-entry.ts
@@ -0,0 +1,22 @@
+import { vikeHandler } from "./server/vike-handler";
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import { createHandler } from "@universal-middleware/hono";
+import { trpcHandler } from "./server/trpc-handler";
+
+const app = new Hono();
+
+app.use("/api/trpc/*", createHandler(trpcHandler)("/api/trpc"));
+
+/**
+ * Vike route
+ *
+ * @link {@see https://vike.dev}
+ **/
+app.all("*", createHandler(vikeHandler)());
+
+export const GET = handle(app);
+
+export const POST = handle(app);
+
+export default process.env.NODE_ENV === "production" ? undefined : app;
