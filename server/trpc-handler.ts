c5ae9e88f96e7ab70aad469c4db30d0da0376479
SlothfulDreams <bensonzheng87901@gmail.com>
2025-08-23 00:29:00 -0400

    another trpc fix

--- server/trpc-handler.ts
+++ server/trpc-handler.ts
@@ -1,7 +1,7 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 // TODO: stop using universal-middleware and directly integrate server middlewares instead and/or use vike-server https://vike.dev/server. (Bati generates boilerplates that use universal-middleware https://github.com/magne4000/universal-middleware to make Bati's internal logic easier. This is temporary and will be removed soon.)
 import type { Get, UniversalHandler } from "@universal-middleware/core";
-import { appRouter } from "@/trpc/server";
+import { appRouter } from "@/trpc/server.js";
 
 export const trpcHandler = ((endpoint) => (request, context, runtime) => {
   return fetchRequestHandler({
